f3235b15fe55d2d2985da7c4403e6e132301c976
nadim <mdnadimmir50@gmail.com>
2024-01-17 13:56:52 +0600

    task 2(a) brightness enhancement

--- Task2/2a.py
+++ Task2/2a.py
@@ -11,3 +11,21 @@ original_image = cv2.imread("./Fractured Spine 746x976.tif", cv2.IMREAD_GRAYSCAL
 plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)) #Displays the image using Matplotlib, converting it to RGB format for visualization.
 plt.title("The Original Image")
 plt.show()
+
+
+
+# Enhancing Brightness of a specific range of Gray Levels
+
+range_start, range_end, add_brightness = 10, 50, 40
+height, width = original_image.shape
+image = original_image
+
+for r in range(width):
+    for c in range(height):
+        if (image[c, r] >= range_start and image[c, r] <= range_end):
+            image[c, r] += add_brightness
+        image[c, r] = 255 if image[c, r] > 255 else image[c, r]
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("The Enhanced Image") 
+plt.show()
