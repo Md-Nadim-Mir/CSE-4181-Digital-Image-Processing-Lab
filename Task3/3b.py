2a39fa306cee21f89e3ba5691c4ed179f8ee3632
nadim <mdnadimmir50@gmail.com>
2024-01-17 16:43:35 +0600

    all everything is complete use python and jupiter both

--- Task3/3b.py
+++ Task3/3b.py
@@ -1,11 +1,16 @@
 #Task 3 - Spatial Domain Smoothing Filters
 #Task 3(b): Take grayscale image of size 512x512, use different size of mask (3x3, 5x5, 7x7) with average filter for noise suppression & observe their performance in term of PSNR
+
+
 #Importing the Libraries
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
 #Function for Average Spatial Filter
 #Function: output_image = cv2.blur(input_image, (kernel_size, kernel_size))
+
 def average_filter(image, mask_size):
     filtered_image = image.copy()
     height, width = filtered_image.shape
@@ -20,7 +25,10 @@ def average_filter(image, mask_size):
                         filtered_image[r, c] += (image[r + x, c + y] / weight)
     
     return np.uint8(filtered_image)
+
+
 #Function for adding Salt & Pepper Noise
+
 def add_salt_pepper_noise(image, percent):
     noisy_image = image.copy()
     noise_amount = (image.shape[0] * image.shape[1]) * (percent / 100)
@@ -32,8 +40,11 @@ def add_salt_pepper_noise(image, percent):
         noisy_image[index[0], index[1]], noisy_image[index[2], index[3]] = 0, 255
 
     return noisy_image
+
+
 #Function for calculating PSNR (Peak Signal to Noise Ratio)
 #Function: psnr = cv2.PSNR(image1, image2)
+
 def compute_psnr(image1, image2):
     image1, image2 = np.float64(image1), np.float64(image2)
     mse = np.mean((image1 - image2) ** 2) 
@@ -42,14 +53,18 @@ def compute_psnr(image1, image2):
     psnr = 20 * np.log10(255.0) - 10 * np.log10(mse)
     
     return round(psnr, 2)
+
+
 #Adding Salt & Pepper Noise to the Image
 #Loading the Image
+
 character_image = cv2.imread("./Characters Test Pattern 688x688.tif", 0)
 plt.imshow(character_image, cmap = "gray")
 plt.title("Original Image")
 plt.show()
 
 #Adding Salt & Pepper Noise to the Image
+
 noisy_character_image = add_salt_pepper_noise(character_image, 15)
 plt.imshow(noisy_character_image, cmap = "gray")
 plt.title("The Noisy Image")
@@ -59,6 +74,8 @@ print(f"PSNR = {psnr}")
 
 #PSNR = 9.58
 #Applying average filter with (3x3, 5x5, 7x7) mask
+
+
 for mask_size in range(3, 8, 2):
     avg_character_image = average_filter(noisy_character_image, mask_size)
     plt.imshow(cv2.cvtColor(avg_character_image, cv2.COLOR_BGR2RGB))
