420322d6ba05d59d1e076f0988005309fea542c8
nadim <mdnadimmir50@gmail.com>
2024-01-17 13:10:49 +0600

    task1 1(c)

--- 1c.py
+++ 1c.py
@@ -1,23 +1,36 @@
 #Task 1 - Resolution, Histogram & Thresholding
 #Task 1(c): Take a grayscale image of size 512x512, illustrate the Histogram of the image & make Single Threshold Segmentation observed from the histogram
+
 #Importing the Libraries
-import cv2
+
+import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+
 #Function for generating Histogram
+
 def generate_histogram(image):
-    gray_levels_count = np.zeros(256)
+    gray_levels_count = np.zeros(256) # Creates an array to store the count of each grayscale level (0-255), initially filled with zeros.
+    
     height, width = image.shape
 
     for r in range (width):
         for c in range(height):
-            gray_levels_count[image[c, r]] += 1
+            gray_levels_count[image[c, r]] += 1 #Increments the count of the corresponding grayscale level in the gray_levels_count array.
 
-    plt.bar(range(256), gray_levels_count, width = 1.0, color = "gray")
+    plt.bar(range(256), gray_levels_count, width = 1.0, color = "gray") #Creates a bar chart using Matplotlib, where the x-axis represents grayscale levels (0-255) and the y-axis represents the count of each level.
+    
     plt.title("The Histogram of the Image")
     plt.show()
+    
+    
 #Loading the Original Image
+
 original_images = cv2.imread("./Skeleton 750x1482.tif", cv2.IMREAD_GRAYSCALE)
-plt.imshow(cv2.cvtColor(original_images, cv2.COLOR_BGR2RGB))
+
+plt.imshow(cv2.cvtColor(original_images, cv2.COLOR_BGR2RGB))#Displays the image using Matplotlib, converting it to RGB format for visualization.
+
 plt.title("The Skull Image")
 plt.show()
